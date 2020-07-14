df3b0cf43fa28ac38468ad4c4608b06e8e2f0b52
Gabriel <gabrielelfassi@yahoo.fr>
2020-07-14 11:49:06 +0200

    unix venv added

--- modules/ocr/__init__.py
+++ modules/ocr/__init__.py
@@ -1,13 +1,16 @@
 import cv2
 from PIL import Image
 import pytesseract
+import platform
+
 
 # -----------------
 # -- Main config --
 # -----------------
 
-TESS_PATH='venv/Lib/appdata/local/Tesseract-OCR/tesseract.exe' # TODO: change path
-pytesseract.pytesseract.tesseract_cmd = TESS_PATH
+if platform.system() == 'Windows':
+    TESS_PATH='venv/Lib/appdata/local/Tesseract-OCR/tesseract.exe' # TODO: change path
+    pytesseract.pytesseract.tesseract_cmd = TESS_PATH
 FILENAME='.tmp/screen.png'
 
 
